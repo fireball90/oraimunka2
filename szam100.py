83135003c1b7f13120042fd51ec525ad706d8f6d
Tanuló 301-17 <301-17@KKSZKI.LOCAL>
2021-11-30 18:34:32 +0100

    szamolos 100 alatt

---
+++ szam100.py
@@ -0,0 +1,12 @@
+print(":::::::::::::::::::::::::::::::::::::::::::::")
+
+i=0
+while i<=100:
+    szam=int(input("Kérem a számot: "))
+    i=i+szam
+
+print("Az összeg meghaladta a százat -> "+str(i))
+
+print(":::::::::::::::::::::::::::::::::::::::::::::")
+
+#mészáros balázs
