16dd31752f38375252248014e2604fe27095bd6e
Tanuló 301-17 <301-17@KKSZKI.LOCAL>
2021-11-30 18:13:07 +0100

    hatvanyos feladat

---
+++ hatvany.py
@@ -0,0 +1,7 @@
+hatvanyAlap=int(input("Kérem a hatvány alapját: "))
+kitevo=int(input("Kérem a hatvány kitevőt: "))
+hatvanyErtek=hatvanyAlap**kitevo
+
+print("-------------------------------------------")
+print("Az eredmény: "+str(hatvanyErtek))
+print("-------------------------------------------")
