aabc85d8d8ece22869c480ea597f474f7a2d9ad0
Tanuló 301-17 <301-17@KKSZKI.LOCAL>
2021-11-30 18:23:28 +0100

    szamegyenes

---
+++ szamegyenes.py
@@ -0,0 +1,7 @@
+print("__________________________________________")
+elso=int(input("Kérem az első számot: "))
+masodik=int(input("Kérem a második számot: "))
+
+tavolsag=abs(elso-masodik)
+print("__________________________________________")
+print("A számegyenesen egymástól való távolságuk: "+str(tavolsag))
