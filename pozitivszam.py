a0349fa3a8ea14e399df488c5d55f24ec93e4558
Tanuló 301-17 <301-17@KKSZKI.LOCAL>
2021-11-30 18:19:38 +0100

    pozitiv szam feladat

---
+++ pozitivszam.py
@@ -0,0 +1,8 @@
+print("___________________________________")
+while True:
+    pozitiv=int(input("Kérek egy pozitív számot: "))
+    if pozitiv<0:
+        print("Negatív szám!")
+        break
+    else:
+        print(str(pozitiv))
